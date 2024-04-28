64a9d1de286b2a4792279cfd05a54a9acfe8a982
Centralice <108938330+Centralice@users.noreply.github.com>
2024-04-28 19:52:07 +0200

    js-done

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,37 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const inputEl = document.querySelector('input');
+const createBtn = document.querySelector('button[data-create]');
+const destroyBtn = document.querySelector('button[data-destroy]');
+const boxesEl = document.querySelector('#boxes');
+
+function createBoxes(amount) {
+  let size = 0;
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    size += 10;
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesEl.append(box);
+  }
+}
+
+createBtn.addEventListener('click', handleCreate);
+destroyBtn.addEventListener('click', destroyBoxes);
+
+function handleCreate(event) {
+  const amount = inputEl.value;
+
+  if (inputEl.value >= 1 && inputEl.value <=100) {
+    createBoxes(amount);
+    inputEl.value = '';
+  }
+}
+
+function destroyBoxes(event) {
+  boxesEl.innerHTML = "";
+}
